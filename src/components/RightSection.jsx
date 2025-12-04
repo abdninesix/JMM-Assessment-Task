307dba4e5b27f295f39980c683cda8eade388f8b
abdninesix <mohabd1995@gmail.com>
2025-12-04 14:24:50 +0500

    Updated

--- src/components/RightSection.jsx
+++ src/components/RightSection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const RightSection = () => {
     return (
         <section className='w-full lg:w-1/2 hidden lg:flex flex-col items-center p-24 justify-center bg-linear-to-br from-sky-400 to-blue-950 rounded-2xl'>
-            {/* Cards */}
+            {/* Image */}
                 <img src='/content.png' alt='content' className='scale-110 animate-slide' />
             {/* Text */}
             <div className='space-y-4 text-center text-white'>
